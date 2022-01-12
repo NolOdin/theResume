ad54b5414c6896f7c7786ea5bc367e93532ab631
Nol.Odin <76630229+NolOdin@users.noreply.github.com>
2022-01-12 04:11:33 +0300

    Add files via upload

---
+++ src/components/spinner.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+    	<div className="rs-container">
+		 <div className="spinner-container">
+		  <div className="spinner-block"></div>
+		  <div className="spinner-block"></div>
+		  <div className="spinner-block"></div>
+		  <div className="spinner-block"></div>
+		  <div className="spinner-block"></div>
+		  <div className="spinner-block"></div>
+		  <div className="spinner-block"></div>
+		  <div className="spinner-block"></div>
+		  <div className="spinner-block"></div>
+		  <div className="spinner-block"></div>
+		  <div className="spinner-block"></div>
+		  <div className="spinner-block"></div>
+		  <div className="spinner-block"></div>
+		  <div className="spinner-block"></div>
+		  <div className="spinner-block"></div>
+		  <div className="spinner-block"></div>
+		</div>
+	</div>
+    );
+};
+
+
+
+export default Spinner;
